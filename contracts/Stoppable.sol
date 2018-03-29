face3696dcef53184e155e64c34020ef4be97d6f
SangphilKim <sangphil.kim@hotmail.com>
2018-03-29 15:26:47 +0900

    add Stoppable.sol

---
+++ contracts/Stoppable.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.20;
+
+import "./Owned.sol";
+
+/**
+ *Base for contracts that can stopped/resumed.
+ */
+
+ contract Stoppable is Owned {
+
+    bool public running;
+
+    function Stoppable() {
+        running = true;
+    }
+
+    modifier onlyIfRunning {
+        require(running);
+        _;
+    }
+
+    function runSwitch(bool onOff) onlyOwner returns (bool success) {
+        running = onOff;
+        return true;
+    }
+
+ }
