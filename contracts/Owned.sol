f10392b88d7e1603196511a65c38d7b768432b3c
SangphilKim <sangphil.kim@hotmail.com>
2018-04-04 15:17:11 +0900

    separate Owned.sol and Petowner.sol

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -6,48 +6,31 @@ pragma solidity ^0.4.20;
 
 contract Owned {
 
-    event LogNewOwner(string oldOwnerName, address oldOwner, string newOwnerName, address newOwner);
-    event LogDeleteOwner(string killOwnerName, address killOwner);
+    event LogNewOwner(address oldOwner, address newOwner);
+    event LogDeleteOwner(address deleteOwner);
 
     address public owner;
-    string public ownerName;
-    string public ownerEmail;
-    string public ownerPhone;
 
     modifier onlyOwner {
         require(owner == msg.sender);
         _;
     }
 
-    function Owned(string _ownerName, string _ownerEmail, string _ownerPhone) public {
+    function Owned() public {
         owner = msg.sender;
-        ownerName = _ownerName;
-        ownerEmail = _ownerEmail;
-        ownerPhone = _ownerPhone;
     }
 
-    function changeOwner(address _newOwner, string _newOwnerName, string _newOwnerEmail, string _newOwnerPhone) public onlyOwner {
+    function changeOwner(address _newOwner) public onlyOwner {
         require(_newOwner != 0);
-        emit LogNewOwner(ownerName, owner, _newOwnerName, _newOwner);
+        emit LogNewOwner(owner, _newOwner);
 
         owner = _newOwner;
-        ownerName = _newOwnerName;
-        ownerEmail = _newOwnerEmail;
-        ownerPhone = _newOwnerPhone;
     }
 
-    function deleteOwner(address outOwner) public onlyOwner {
-        emit LogDeleteOwner(ownerName, outOwner);
+    function deleteOwner(address _deleteOwner) public onlyOwner {
+        emit LogDeleteOwner(_deleteOwner);
 
         owner = 0x0;
-        delete ownerName;
-        delete ownerEmail;
-        ownerPhone = ""; //which is more efficient?
     }
-
-    // function ownerInfo(string _ownerName) public view returns (address _infoAddress, string _infoEmail, string _infoPhone){
-    //     require(ownerName == _ownerName);
-    //     return (owner, ownerEmail, ownerPhone);
-    // }
 
 }
