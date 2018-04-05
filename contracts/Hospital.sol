5d7f69306426ea631e051607de0885d97c67644b
SangphilKim <sangphil.kim@hotmail.com>
2018-04-05 18:39:20 +0900

    modify Hospital.sol

--- contracts/Hospital.sol
+++ contracts/Hospital.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.4.20;
 
+import "./Doctor.sol";
+import "./Pet.sol";
+
 /**
  *Base for contract that work with the abstraction of animal hospital.
  */
 
-contract Hospital {
+contract Hospital is Doctor, Pet {
+
+    function Hospital() public {
+
+    }
 
 }
