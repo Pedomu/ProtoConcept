72f0b445a889f305bf1b9acc54d4770157cee84a
SangphilKim <sangphil.kim@hotmail.com>
2018-04-05 14:24:54 +0900

    add deletePet in Pet.sol

--- contracts/Pet.sol
+++ contracts/Pet.sol
@@ -5,11 +5,11 @@ import "./Petowner.sol";
 contract Pet is Petowner {
 
     event PetInformation(string petNameInfo, uint petAgeInfo, address petAndOwnerAddress, string petOwnerNameInfo, string petOwnerPhoneInfo);
+    event PetDelete(string deletePetName, address deletePetAddress);
 
     string public petName;
     address public petAddress;
     uint public petAges;
-
 
     function Pet() public {
         petAges = 0;
@@ -19,8 +19,17 @@ contract Pet is Petowner {
         petName = _petName;
         petAddress = msg.sender;
         petAges = _petAges;
-        
+
         emit PetInformation(petName, petAges, petAddress, petOwnerName, petOwnerPhone);
     }
 
+    function deletePet() public onlyPetOwner {
+        require(msg.sender == petAddress);
+
+        emit PetDelete(petName, petAddress);
+
+        delete petName;
+        petAddress = 0x0;
+        petAges = 0;
+    }
 }
