0aade880c716a9448e5e298c9e48a22705d33249
SangphilKim <sangphil.kim@hotmail.com>
2018-04-12 17:50:50 +0900

    add medical info

--- contracts/Medical.sol
+++ contracts/Medical.sol
@@ -5,6 +5,12 @@ pragma solidity ^0.4.20;
  */
 
 contract Medical {
+
+    uint public medicalChart;
+    //medical info_two digit?
+    //first digit is category(1*: visit list, 2*: surgery list, ...)
+    //second digit is medical details in category (11: ultrasonic wave, 12: cheak blood, ...)
+
     function Medical() public {
 
     }
